eae9d4c4ab56b5d32cf636d991c3f01a2d8a5ff1
miketvo <ikxplicitie@gmail.com>
2023-07-14 11:23:10 +0700

    Template Customization

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "miketvo/scoop-miketvo:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
